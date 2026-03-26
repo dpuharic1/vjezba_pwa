4b1b38c28ee193d31848e5c4fcb192af316cdd5d
Duje Puharić <dpuh1@outlook.com>
2026-03-26 22:58:02 +0100

    Add initial version of vjezba1.php with basic content

---
+++ vjezba1.php
@@ -0,0 +1,10 @@
+<?php
+$naslov = "Vježba 1";
+$autor = "Duje Puharić";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio je $autor</p>";
+echo '<a href="https://hrt.hr" target="_blank">Posjetite HRT</a>';
+?>
+
+<!-- Naziv datoteke: vjezba1.php -->
